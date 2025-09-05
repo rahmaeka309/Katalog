0e49cc4f5b5c8a6ffeac4b52b3bf8148641b6f05
rahmaeka309 <138545111+rahmaeka309@users.noreply.github.com>
2025-09-05 09:30:30 +0700

    Add files via upload

--- lib/koneksi.php
+++ lib/koneksi.php
@@ -1,1 +1,9 @@
-
+<?php 
+define('url_temp',"http://localhost/katalog/asset/");
+$conn = new mysqli('localhost','root','','db_katalog');
+if($conn){
+	//echo"Berhasil terhubung dengan database";
+}else{
+	echo"Terjadi kesalahan, silahkan analisa sendiri";
+}
+ ?>
